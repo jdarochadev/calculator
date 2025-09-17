38ab687463fd6198f2b814c173daf2838e90ba51
Joao Victor Cruz da Rocha <jdarochadev@gmail.com>
2025-09-17 19:44:25 -0300

    termino

--- Calculator.java
+++ Calculator.java
@@ -24,21 +24,21 @@ public class Calculator {
         switch (operator) {
             case '+':
                 result = num1 + num2;
-                System.out.println("Result: " + result);
+                System.out.println("Resultado: " + result);
                 break;
             case '-':
                 result = num1 - num2;
-                System.out.println("Result: " + result);
+                System.out.println("Resultado: " + result);
                 break;
             case '*':
                 result = num1 * num2;
-                System.out.println("Result: " + result);
+                System.out.println("Resultado: " + result);
                 break;
             case '/':
                 
                 if (num2 != 0) {
                     result = num1 / num2;
-                    System.out.println("Result: " + result);
+                    System.out.println("Resultado: " + result);
                 } else {
                     System.out.println("Error: Divisao por Zero nao e permitido.");
                 }
